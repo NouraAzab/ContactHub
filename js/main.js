aa49014276de512f446447253c3eb1b398af094a
NouraAzab <noura.azab1357@gmail.com>
2025-12-08 16:12:53 +0200

    make the card of the contact

---
+++ js/main.js
@@ -0,0 +1,8 @@
+//todo remove the scroll in the form
+//todo      add contact   =>  add    in md : 768px   [responsie]
+//todo    random color    =>  contactCard  head-of-contact
+
+
+// --------------------
+//todo style : specific color for friends | family      [group]
+//todo   rondom color for div of img of contact
