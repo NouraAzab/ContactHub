959ea2d79f3a31d4d0696325983ec1ecd043ee47
NouraAzab <noura.azab1357@gmail.com>
2025-12-10 12:31:40 +0200

    make the sidebar [fav - Emergency] stack

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
-//todo remove the scroll in the form
-//todo      add contact   =>  add    in md : 768px   [responsie]
-//todo    random color    =>  contactCard  head-of-contact
-
+//todo "done"   :   remove the scroll in the form
+//todo          :   add contact   =>  add    in md : 768px   [responsie]
+//todo          :   random color    =>  contactCard  head-of-contact
+//todo "done"   :  scroll for sidebar emergency - Fav [0 : md   responsie]
+//todo VIP   : Not fav yet    ,  Not emer yet  ,   Not Contacts yet  if the arr empty ^^
 
 // --------------------
 //todo style : specific color for friends | family      [group]
